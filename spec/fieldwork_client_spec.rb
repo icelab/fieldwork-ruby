2570b7445a4ed78dcd7e79e44276128491f4f54c
Andrew Croome <andrewcroome@gmail.com>
2016-09-29 12:21:11 +1000

    Add initial specs for tracking events.

--- spec/fieldwork_client_spec.rb
+++ spec/fieldwork_client_spec.rb
@@ -17,6 +17,15 @@ describe Fieldwork::Client do
   end
 
   describe "#track_event" do
+    let(:api_request_headers) {
+      {
+        'Accept'=>'*/*',
+        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+        'Content-Type'=>'application/json',
+        'User-Agent'=>'Faraday v0.9.2'
+      }
+    }
+
     describe "preflight" do
       it "ensures that a configuration is available" do
         expect { described_class.new(configuration: nil).track_event("foo", {bar: "baz"}) }.to raise_error(
@@ -38,6 +47,76 @@ describe Fieldwork::Client do
         expect { client.track_event("foo", {bar: "baz"}) }.to raise_error(
           Fieldwork::ConfigurationError, "Please configure a project_public_key before tracking events"
         )
+      end
+    end
+
+    context "with valid credentials" do
+      before do
+        stub_request(:post, "http://api.dofieldwork.com/api/v1/projects/project-id/events")
+          .with(
+            body: "{\"name\":\"user_created\",\"properties\":{\"email\":\"user@example.com\"}}",
+            headers: api_request_headers.merge('Authorization'=>'Basic cHJvamVjdC1wdWJsaWMta2V5Og==')
+          ).to_return(status: 200, body: '{"status": "ok"}', headers: {})
+      end
+
+      it "sends an event and properties to the fieldwork api and returns status ok" do
+        client = described_class.new(
+          configuration: Fieldwork::Configuration.new(
+            project_id: "project-id", project_public_key: "project-public-key"
+          )
+        )
+
+        expect(
+          client.track_event("user_created", {email: "user@example.com"})
+        ).to eq(status: "ok")
+      end
+    end
+
+    context "with invalid credentials" do
+      before do
+        stub_request(:post, "http://api.dofieldwork.com/api/v1/projects/project-id/events")
+          .with(
+            body: "{\"name\":\"user_created\",\"properties\":{\"email\":\"user@example.com\"}}",
+            headers: api_request_headers.merge('Authorization'=>'Basic aW52YWxpZC1rZXk6')
+          ).to_return(
+            status: 422,
+            body: "{\"status\":\"error\",\"message\":\"Incorrect project id or public key. Please double check your project's id and public key\"}",
+            headers: {}
+          )
+      end
+
+      it "sends an event and properties to the fieldwork api but raises an api error" do
+        client = described_class.new(
+          configuration: Fieldwork::Configuration.new(
+            project_id: "project-id", project_public_key: "invalid-key"
+          )
+        )
+
+        expect{
+          client.track_event("user_created", {email: "user@example.com"})
+        }.to raise_error Fieldwork::ApiError, "Incorrect project id or public key. Please double check your project's id and public key"
+      end
+    end
+
+    context "with a missing endpoint" do
+      before do
+        stub_request(:post, "http://api.dofieldwork.com/api/v1/projects/project-id/events")
+          .with(
+            body: "{\"name\":\"user_created\",\"properties\":{\"email\":\"user@example.com\"}}",
+            headers: api_request_headers.merge('Authorization'=>'Basic cHJvamVjdC1wdWJsaWMta2V5Og==')
+          ).to_return(status: 404, body: '', headers: {})
+      end
+
+      it "sends an event and properties to the fieldwork api and but raises an api error" do
+        client = described_class.new(
+          configuration: Fieldwork::Configuration.new(
+            project_id: "project-id", project_public_key: "project-public-key"
+          )
+        )
+
+        expect{
+          client.track_event("user_created", {email: "user@example.com"})
+        }.to raise_error Fieldwork::ApiError, "404 endpoint not found: http://api.dofieldwork.com/api/v1/projects/project-id/events"
       end
     end
   end
